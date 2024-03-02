463dc44711cea04dc4fa8652cd0339f8840fb3be
Rafael Soto <rafael.soto.iv@gmail.com>
2024-03-02 13:53:01 -0600

    Pyinstaller update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\teran\PycharmProjects\PublicCitizen_Project\venv"
+set "VIRTUAL_ENV=C:\Users\teran\PycharmProjects\TCEQ Data Organizer\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
